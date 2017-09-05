5ff7486149957092e1c03353fdcec157ecf710c3
T-vK <t-v.k@gmx.net>
2017-09-05 18:30:04 +0200

    fist audio tests working; notworking not functional yet

--- audio-test.js
+++ audio-test.js
@@ -1,21 +1,37 @@
-const net = require('net')
 const mic = require('mic')
 const Speaker = require('speaker')
 
-const micInstance = mic({
-    rate: '16000',
-    channels: '1',
-    debug: true,
-    exitOnSilence: 6
+const micInstance = mic({ // arecord -D hw:0,0 -f S16_LE -r 44100 -c 2
+    device: 'hw:0,0',           //   -D hw:0,0
+    encoding: 'signed-integer', //             -f S
+    bitwidth: '16',             //                 16
+    endian: 'little',           //                   _LE
+    rate: '44100',              //                       -r 44100
+    channels: '2',              //                                -c 2
+    debug: true
+    //exitOnSilence: 6
 })
 const micInputStream = micInstance.getAudioStream()
-const speakerInstance = new Speaker({
-    channels: 2,          // 2 channels
-    bitDepth: 16,         // 16-bit samples
-    sampleRate: 44100     // 44,100 Hz sample rate
-})
-console.log('about to start')
-micInputStream.pipe(speakerInstance)
-console.log('started')
 
-process.stdin.resume()
+const speakerInstance = new Speaker({ // | aplay -D plughw:CARD=0,DEV=0
+    channels: 2,
+    bitDepth: 16,
+    sampleRate: 44100,
+    signed: true,
+    device: 'plughw:NVidia,7'
+})
+speakerInstance.on('open', ()=>{
+    console.log("Speaker received stuff")
+})
+
+
+micInputStream.pipe(speakerInstance)
+micInputStream.on('data', data => {
+    console.log("Recieved Input Stream: " + data.length)
+})
+micInputStream.on('error', err => {
+    cosole.log("Error in Input Stream: " + err)
+})
+micInstance.start()
+
+console.log('Started')
--- index.js
+++ index.js
@@ -6,31 +6,57 @@ const package = require('./package.json')
 
 program
   .version(package.version)
-  .option('-c, --connect <hosts>', 'Connect to one or more hosts, (Comma-separated list of hosts. Supports IPs and hostnames.)', hosts=>hosts.split(','))
-  .option('-l, --listen [hosts]', 'Automatically accept connection form these hosts. (Comma-separated list of hosts. Supports IPs and hostnames. Empty= Accept all connections)', hosts=>hosts.split(','), [])
-  .option('-p, --port [port]', 'Default port to be used. (Empty= 80)', 80)
+  .option('-c, --connect <host:port>', 'Connect to a host, (Supports IP:port and hostname:port.)')
+  .option('-l, --listen <port>', 'Automatically accept connections on this port.')
+  .option('-i, --input [device-name]', 'Input device, (Leave empty to use the default recording device.)')
+  .option('-o, --output [device-name]', 'Output device, (Leave empty to use the default playback device.)')
   //.option('-s, --speaker-enabled', 'Speaker enabled initially. (true or false)', true)
   //.option('-m, --microphone-enabled', 'Microphone enabled initially. (true or false)', true)
   .parse(process.argv)
 
-console.log('Connect-hosts: ' + program.connect)
-console.log('Listen-hosts: ' + program.listen)
-console.log('Port: ' + program.port)
+console.log('--connect: ' + program.connect)
+console.log('--listen: ' + program.listen)
+console.log('--input: ' + program.input)
+console.log('--output: ' + program.output)
 
 const mode = !program.connect ? 'listen' : 'connect'
 
-const micInstance = mic({
-    rate: '16000',
-    channels: '1',
-    debug: true,
-    exitOnSilence: 6
-})
+let speakerConfig = { // | aplay -D plughw:NVidia,7
+    //device: program.output, // -D plughw:NVidia,7
+    channels: 2,
+    bitDepth: 16,
+    sampleRate: 44100,
+    signed: true
+}
+
+let micConfig = {       // arecord -D hw:0,0 -f S16_LE -r 44100 -c 2
+    //device: program.input,    // -D hw:0,0
+    encoding: 'signed-integer', //           -f S
+    bitwidth: '16',             //               16
+    endian: 'little',           //                 _LE
+    rate: '44100',              //                     -r 44100
+    channels: '2',              //                              -c 2
+    debug: true
+}
+if (program.input)
+    micConfig.device = program.input
+
+const micInstance = mic(micConfig)
 const micInputStream = micInstance.getAudioStream()
-const speakerInstance = new Speaker({
-    channels: 2,          // 2 channels
-    bitDepth: 16,         // 16-bit samples
-    sampleRate: 44100     // 44,100 Hz sample rate
+const speakerInstance = new Speaker(speakerConfig)
+
+speakerInstance.on('open', ()=>{
+    console.log("Speaker received stuff")
 })
+
+micInputStream.pipe(speakerInstance)
+//micInputStream.on('data', data => {
+//    console.log("Recieved Input Stream: " + data.length)
+//})
+micInputStream.on('error', err => {
+    cosole.log("Error in Input Stream: " + err)
+})
+micInstance.start()
 
 
 const server = net.createServer(socket=>{
@@ -42,16 +68,24 @@ console.log('Mode: ' + mode)
 
 if (mode === 'listen') {
     const server = net.createServer(socket=>{
+        //micInputStream.pipe(socket) 
+        //socket.pipe(speakerInstance)
+    })
+    server.on('error', err => {
+        throw err
+    })
+    server.on('connection', socket => {
+        console.log('A client has connected.')
         micInputStream.pipe(socket) 
         socket.pipe(speakerInstance)
-    }) 
-    server.listen(program.port, ()=>{
-        console.log('Server: Connected')
+    })
+    server.listen(program.listen, ()=>{
+        console.log('Server is listening')
     })
 } else {
     const client = new net.Socket()
-    client.connect(program.port, {host: program.listen[0], port: program.port}, ()=>{
-        console.log('Client: Connected')
+    client.connect({host: program.connect.split(':')[0], port: program.connect.split(':')[1]}, ()=>{
+        console.log('Client connected')
         micInputStream.pipe(client)
         client.pipe(speakerInstance)
     })
