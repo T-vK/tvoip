5ea2c9e9a5c19ded628c5c0969e9283e7837c7a3
T-vK <t-v.k@gmx.net>
2017-09-06 08:45:20 +0200

    fixed speaker path

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const program = require('commander')
 const net = require('net')
 const mic = require('mic')
-//const Speaker = require('speaker')
-const Speaker = require('./node-speaker')
+const Speaker = require('speaker')
+//const Speaker = require('./node-speaker')
 const package = require('./package.json')
 
 program
